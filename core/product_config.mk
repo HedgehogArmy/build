206e2b0f6828becd8c40bfd338790105fa178ffb
Steve Kondik <shade@chemlab.org>
2017-08-22 23:16:15 +0200

    Fix fastpath code for sonic builds
    Change-Id: I3207dc1c8870548bc6536a9a401b712bce706c4e

--- core/product_config.mk
+++ core/product_config.mk
@@ -181,7 +181,7 @@ all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
  ifneq ($(SONIC_BUILD),)
-    all_product_configs := device/*/$(SONIC_BUILD)/sonic.mk
+    all_product_configs := $(shell ls device/*/$(SONIC_BUILD)/sonic.mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
